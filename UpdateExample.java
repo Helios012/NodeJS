bad725a83a4604169c22ea78e8878188c83ec133
Helios012 <adithyapathakoti@gmail.com>
2025-05-09 00:25:58 +0530

    Update UpdateExample.java

--- UpdateExample.java
+++ UpdateExample.java
@@ -4,7 +4,7 @@ public class UpdateExample {
     public static void main(String[] args) {
         try {
             Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/company", "root", "Adithya!24");
+                "jdbc:mysql://localhost:3306/company", "root", "root");
             Statement stmt = conn.createStatement();
             String sql = "UPDATE employees SET department='HR' WHERE id=101";
             int rows = stmt.executeUpdate(sql);
@@ -17,3 +17,4 @@ public class UpdateExample {
 }
 //compile--javac -cp .;mysql-connector-j-9.3.0.jar UpdateExample.java
 //run--java -cp .;mysql-connector-j-9.3.0.jar UpdateExample
+/*make a database company in mysql and create a table employees with ID,Name,department as parameters and enter some data starting with the ID from 101 */
